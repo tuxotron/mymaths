5c70107dfafda2a4bb5caf9d8c19606587c8ed23
tuxotron <tuxotron@gmail.com>
2017-10-07 23:57:04 -0400

    added levels and subtraction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AdditionComponent } from './addition/addition.component';
 import { GrowlModule }  from 'primeng/primeng';
 
 import {MessageService} from 'primeng/components/common/messageservice';
+import { SubtractionComponent } from './subtraction/subtraction.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdditionComponent
+    AdditionComponent,
+    SubtractionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     GrowlModule,
     RouterModule.forRoot([
-      {
-        path: 'addition',
-        component: AdditionComponent
-      }
+      {path: '4A', component: AdditionComponent, data: {level: '4'}},
+      {path: '5A',component: AdditionComponent, data: {level: '5'}},
+      {path: '6A',component: AdditionComponent,data: {level: '6'}},
+      {path: '7A',component: AdditionComponent,data: {level: '7'}},
+      {path: '8A',component: AdditionComponent,data: {level: '8'}},
+      {path: '9A',component: AdditionComponent,data: {level: '9'}},
+      {path: '10A',component: AdditionComponent,data: {level: '10'}},
+      {path: '11A',component: AdditionComponent,data: {level: '11'}},
+      {path: '12A',component: AdditionComponent,data: {level: '12'}},
+      {path: '13A',component: AdditionComponent,data: {level: '13'}},
+      {path: '14A',component: AdditionComponent,data: {level: '14'}},
+      {path: '15A',component: AdditionComponent,data: {level: '15'}},
+      {path: '16A',component: AdditionComponent,data: {level: '16'}},
+      {path: '17A',component: AdditionComponent,data: {level: '17'}},
+      {path: '18A',component: AdditionComponent,data: {level: '18'}},
+
+      {path: '4B', component: SubtractionComponent, data: {level: '4'}},
+      {path: '5B',component: SubtractionComponent, data: {level: '5'}},
+      {path: '6B',component: SubtractionComponent,data: {level: '6'}},
+      {path: '7B',component: SubtractionComponent,data: {level: '7'}},
+      {path: '8B',component: SubtractionComponent,data: {level: '8'}},
+      {path: '9B',component: SubtractionComponent,data: {level: '9'}},
+      {path: '10B',component: SubtractionComponent,data: {level: '10'}},
+      {path: '11B',component: SubtractionComponent,data: {level: '11'}},
+      {path: '12B',component: SubtractionComponent,data: {level: '12'}},
+      {path: '13B',component: SubtractionComponent,data: {level: '13'}},
+      {path: '14B',component: SubtractionComponent,data: {level: '14'}},
+      {path: '15B',component: SubtractionComponent,data: {level: '15'}},
+      {path: '16B',component: SubtractionComponent,data: {level: '16'}},
+      {path: '17B',component: SubtractionComponent,data: {level: '17'}},
+      {path: '18B',component: SubtractionComponent,data: {level: '18'}},
     ])
   ],
   providers: [MessageService],
--- src/app/subtraction/subtraction.component.ts
+++ src/app/subtraction/subtraction.component.ts
@@ -1,18 +1,18 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {MessageService} from 'primeng/components/common/messageservice';
-import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
+import {MessageService} from "primeng/components/common/messageservice";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-addition',
-  templateUrl: './addition.component.html',
-  styleUrls: ['./addition.component.css']
+  selector: 'app-subtraction',
+  templateUrl: './subtraction.component.html',
+  styleUrls: ['./subtraction.component.css']
 })
-export class AdditionComponent implements OnInit, OnDestroy {
+export class SubtractionComponent implements OnInit, OnDestroy {
 
   sub: Subscription;
   score: number;
-  addition: string;
+  subtraction: string;
   operand1: number;
   operand2: number;
   result: string;
@@ -33,7 +33,7 @@ export class AdditionComponent implements OnInit, OnDestroy {
 
     console.log("level: " + this.level);
 
-    this.addition = this.getRandomAddition(this.level);
+    this.subtraction = this.getRandomSubstraction(this.level);
 
   }
 
@@ -45,18 +45,18 @@ export class AdditionComponent implements OnInit, OnDestroy {
 
     console.log(value)
 
-    if ((this.operand1 + this.operand2) === Number(value)) {
+    if ((this.operand1 - this.operand2) === Number(value)) {
       this.score += 1;
       this.messageService.add({severity: 'success', summary: 'Correct!'});
     } else {
       this.messageService.add({severity: 'error', summary: 'Sorry :('});
     }
 
-    this.addition = this.getRandomAddition(this.level);
+    this.subtraction = this.getRandomSubstraction(this.level);
     this.result = '';
   }
 
-  getRandomAddition(topNumber: number): string {
+  getRandomSubstraction(topNumber: number): string {
 
     var ceiling1 = topNumber > 9 ? 10 : topNumber;
     this.operand1 = Math.floor(Math.random() * ceiling1);
@@ -64,8 +64,13 @@ export class AdditionComponent implements OnInit, OnDestroy {
     var ceiling2 = (topNumber - this.operand1) > 9 ? 10 : (topNumber - this.operand1);
     this.operand2 = Math.floor(Math.random() * ceiling2);
 
-    return this.operand1.toString() + " + " + this.operand2.toString();
+    if (this.operand1 < this.operand2) {
+      var aux = this.operand1;
+      this.operand1 =+ this.operand2;
+      this.operand2 = this.operand1 - aux;
+    }
+
+    return this.operand1.toString() + " - " + this.operand2.toString();
 
   }
-
 }
